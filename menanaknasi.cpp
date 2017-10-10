6c9398450691af17ef89a8d046694ed46c8fccdb
yosuastefanus <yosua.epen@gmail.com>
2017-10-10 22:34:53 +0700

    versi 1.0.0

---
+++ menanaknasi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+cout << "1. ambil panci" << endl;
+cout << "2. ambil nasi" << endl;
+cout << "3. masukkin nasi ke panci" << endl;
+cout << "4. masukkin air ke panci" << endl;
+cout << "5. cuci sampe bersih" << endl;
+cout << "6. buang airnya" << endl;
+cout << "7. masukin air bersih sesuai takaran" << endl;
+cout << "8. masukkin nasi ke magicjer" << endl;
+cout << "9. tunggu sampe masak" << endl;
+cout << "10. nasi siap untuk dimakan" << endl;
+cout << "=======================================" << endl;
+
+return 0;
+}
