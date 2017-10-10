9a10e4ea0f63677574719247eb795dcfb56585fb
yosua stefanus kurniawan <32316011+yosuastefanus@users.noreply.github.com>
2017-10-10 22:43:08 +0700

    Add files via upload

---
+++ menjadisarjana2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+void panggilsd();
+void panggilsmp();
+void panggilsma();
+void panggilkuliah();
+
+int main () {
+panggilsd();
+panggilsmp();
+panggilsma();
+panggilkuliah();
+
+return 0;
+}
+
+void panggilsd() {
+cout << "1. masuk sd kelas 1" << endl;
+cout << "2. naik ke kelas 2" << endl;
+cout << "3. naik ke kelas 3" << endl;
+cout << "4. naik ke kelas 4" << endl;
+cout << "5. naik ke kelas 5" << endl;
+cout << "6. naik ke kelas 6" << endl;
+cout << "7. lulus dari sd" << endl;
+cout << "============================" << endl;
+}
+
+void panggilsmp() {
+cout << "8. masuk smp kelas 7" << endl;
+cout << "9. naik ke kelas 8" << endl;
+cout << "10. naik ke kelas 9" << endl;
+cout << "11. lulus dari smp" << endl;
+cout << "============================" << endl;
+}
+
+void panggilsma() {
+cout << "12. masuk sma kelas 10" << endl;
+cout << "13. naik ke kelas 11" << endl;
+cout << "14. naik ke kelas 12" << endl;
+cout << "15. lulus dari sma" << endl;
+cout << "============================" << endl;
+}
+
+void panggilkuliah() {
+cout << "16. daftar di university" << endl;
+cout << "17. masuk semester 1" << endl;
+cout << "18. naik ke semester 2" << endl;
+cout << "19. naik ke semester 3" << endl;
+cout << "20. naik ke semester 4" << endl;
+cout << "21. naik ke semester 5" << endl;
+cout << "22. naik ke semester 6" << endl;
+cout << "23. naik ke semester 7" << endl;
+cout << "24. naik ke semester 8" << endl;
+cout << "25. sidang" << endl;
+cout << "26. wisuda" << endl;
+cout << "27. menjadi sarjana" << endl;
+cout << "============================" << endl;
+}
